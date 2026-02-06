03cf99611ac628ed7b63be3fb9597d0642e7f71e
Flerys11 <fleuryssh@gmail.com>
2026-02-06 17:31:57 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
