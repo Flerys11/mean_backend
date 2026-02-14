87ea39d4269c64c4b07f757d21e7e280c26110fb
Flerys11 <fleuryssh@gmail.com>
2026-02-14 11:29:39 +0300

    Commit Inscription et login

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(require("./middlewares/error.middleware"));
+app.use("/auth", require("./routes/auth.route"));
+app.use("/utilisateur", require("./routes/utilisateur.route"));
+
+app.use(require("./middlewares/erreur.middlewares"));
 
 module.exports = app;
+
