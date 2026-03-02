f5a514c5abe34008cfc6fb49d77476d865710a7b
Jimmy2038 <119540791+Jimmy2038@users.noreply.github.com>
2026-03-02 19:53:21 +0300

    Update boutique.route.js

--- src/routes/boutique.route.js
+++ src/routes/boutique.route.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 const controller = require("../controllers/Boutique.controller");
 const auth = require("../middlewares/auth.middlewares");
 
-router.use(auth);
 
-router.post("/", controller.create);
+
+
 router.get("/", controller.findAll);
 router.get("/:id", controller.findOne);
+router.use(auth);
+router.post("/", controller.create);
 router.put("/:id", controller.update);
 router.delete("/:id", controller.delete);
 
