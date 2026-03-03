92d58ce44e1585f6403ed1a40c75429aafb97a96
Flerys11 <fleuryssh@gmail.com>
2026-03-03 21:14:29 +0300

    update stock article

--- src/services/Stock.service.js
+++ src/services/Stock.service.js
@@ -3,13 +3,14 @@ const paginationService = require("./Pagination.service");
 const mongoose = require('mongoose');
 
 class StockService {
+
     #getAggregationPipeline() {
         return [
             {
                 $group: {
                     _id: "$id_article",
-                    total_entree: { $sum: "$quantite_entree" },
-                    total_sortie: { $sum: "$quantite_sortie" }
+                    total_entree: {$sum: "$quantite_entree"},
+                    total_sortie: {$sum: "$quantite_sortie"}
                 }
             },
             {
@@ -37,12 +38,61 @@ class StockService {
         ];
     }
 
+    #getAggregationPipelineBoutique(idBoutique = null) {
+        const pipeline = [
+            {
+                $group: {
+                    _id: "$id_article",
+                    total_entree: {$sum: "$quantite_entree"},
+                    total_sortie: {$sum: "$quantite_sortie"}
+                }
+            },
+            {
+                $lookup: {
+                    from: "articles",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "article"
+                }
+            },
+            {$unwind: "$article"}
+        ];
+
+        if (idBoutique) {
+            pipeline.push({
+                $match: {
+                    "article.id_boutique": new mongoose.Types.ObjectId(idBoutique)
+                }
+            });
+        }
+
+        pipeline.push({
+            $project: {
+                nom_article: "$article.nom_article",
+                prix: "$article.prix",
+                description: "$article.description",
+                photo: "$article.photo",
+                id_categorie: "$article.id_categorie",
+                stock_restant: {
+                    $subtract: ["$total_entree", "$total_sortie"]
+                }
+            }
+        });
+
+        return pipeline;
+    }
+
     async create(data) {
         return await Stock.create(data);
     }
 
     async getStockGlobal(options = {}) {
         const pipeline = this.#getAggregationPipeline();
+        return await paginationService.getPaginatedAggregation(Stock, pipeline, options, 10);
+    }
+
+    async getStockByBoutique(idBoutique, options = {}) {
+        const pipeline = this.#getAggregationPipelineBoutique(idBoutique);
         return await paginationService.getPaginatedAggregation(Stock, pipeline, options, 10);
     }
 
