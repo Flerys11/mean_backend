88ef060fa83ecd3f4d0e92602c4352513eb40cca
Flerys11 <fleuryssh@gmail.com>
2026-03-03 11:03:54 +0300

    commit configuration deploiement

--- src/routes/boutique.route.js
+++ src/routes/boutique.route.js
@@ -1,13 +1,10 @@
 const router = require("express").Router();
 const controller = require("../controllers/Boutique.controller");
 const auth = require("../middlewares/auth.middlewares");
-
-
-
+router.use(auth);
 
 router.get("/", controller.findAll);
 router.get("/:id", controller.findOne);
-router.use(auth);
 router.post("/", controller.create);
 router.put("/:id", controller.update);
 router.delete("/:id", controller.delete);
