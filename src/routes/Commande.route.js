f60f2a9fe5789f50f9ba306d6c5ab0dbe47be6d8
Flerys11 <fleuryssh@gmail.com>
2026-03-03 18:01:34 +0300

    configuration

--- src/routes/Commande.route.js
+++ src/routes/Commande.route.js
@@ -5,7 +5,7 @@ const auth = require("../middlewares/auth.middlewares");
 router.use(auth);
 
 router.post("/", controller.create);
-router.get("/", controller.findPending);
+router.get("/:id", controller.findPending);
 router.put("/status/:id", controller.updateVersionToOne);
 router.put("/annuler/:id", controller.cancel);
 router.get("/stats/:type", controller.getStats);
